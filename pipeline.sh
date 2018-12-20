4ea16b31ffb0654f6f05e394092f3a23364cebeb
WhatsARanjit <whatsaranjit@gmail.com>
2018-12-19 19:31:16 -0500

    Travis pipeline add

---
+++ pipeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curl -o terraform.zip -L "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+unzip terraform.zip
+chmod +x terraform
+
+cat << EOF > ~/.terraformrc
+credentials "app.terraform.io" {
+  token = "${TFE_TOKEN}"
+}
+EOF
+
+cat << EOF > workspace.tf
+terraform {
+  backend "remote" {
+    workspaces {
+     prefix = "${TRAVIS_REPO_SLUG}-"
+    }
+  }
+}
+EOF
+
+./terraform init
+./terraform validate
+
+if [[ $TRAVIS_BRANCH == 'master' ]]
+then
+  ./terraform workspace select prod
+  ./terraform apply -auto-approve
+fi
